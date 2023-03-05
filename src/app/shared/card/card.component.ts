43575dbc6dfd82055d73ed5d9b716c35b8fe0afe
Amitabh Jaiswal <64206119+amitabh-jaiswal@users.noreply.github.com>
2023-03-05 19:26:00 +0530

    Added manage feature coming soon tag.

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  @Input() title: string;
+  @Input('card-title') title: string;
   @Input('align-actions') alignActions: 'start' | 'end';
 
   constructor() { }
